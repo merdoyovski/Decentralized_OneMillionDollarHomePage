cb93425128c1e631d681887a342ea45200c5bd51
mertmalaz <mertmalaz@sabanciuniv.edu>
2021-05-24 19:11:42 +0300

    Github page update

---
+++ build/precache-manifest.d982c5c457b6078465d879a960c6379b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "df9bc369d04a74c9351c",
+    "url": "/static/js/main.df9bc369.chunk.js"
+  },
+  {
+    "revision": "316037c10128a9d34c1f",
+    "url": "/static/js/1.316037c1.chunk.js"
+  },
+  {
+    "revision": "df9bc369d04a74c9351c",
+    "url": "/static/css/main.ac74054a.chunk.css"
+  },
+  {
+    "revision": "316037c10128a9d34c1f",
+    "url": "/static/css/1.ada5cdca.chunk.css"
+  },
+  {
+    "revision": "f5cc7ac52817565204e7327e3378e86b",
+    "url": "/index.html"
+  }
+];
